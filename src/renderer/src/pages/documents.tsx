d0539acfec7aee168c75d3f8fa5ac6c89c55cc8e
David Augusto <davidribeiro086@gmail.com>
2023-01-08 16:48:13 -0300

    feat: text editor

--- src/renderer/src/pages/documents.tsx
+++ src/renderer/src/pages/documents.tsx
@@ -1,10 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Editor } from '../components/Editor'
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-notes-400">
-      Document
-      <Link to="/">Acessar documentos</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-notes-300 font-semibold text-xs">TABLE OF CONTENT</span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>Banco de dados</ToC.Section>
+          <ToC.Section>Autenticação</ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-content">
+        <Editor />
+      </section>
     </main>
   )
 }
