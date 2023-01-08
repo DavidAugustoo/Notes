910769a6e1208a98ada1eab8c5245820498d2057
David Augusto <davidribeiro086@gmail.com>
2023-01-07 22:41:35 -0300

    typescript build modifications

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,11 +1,13 @@
-import { resolve } from 'path'
+import path, { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+
+    publicDir: path.resolve('resources')
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
